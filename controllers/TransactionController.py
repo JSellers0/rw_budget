62a29225af8dd3262c2c28c28de3d24cbe9c8649
Sellerman <Jeffrey.Sellers@maritz.com>
2023-03-12 11:09:25 -0400

    just put the models file with the controllers dummy

--- controllers/TransactionController.py
+++ controllers/TransactionController.py
@@ -1,12 +1,12 @@
-import controllers.models.transaction as tr
+from models import Transaction
 from app import db
 
-def insert_transaction(transaction_data: dict) -> tr.Transaction:
+def insert_transaction(transaction_data: dict) -> Transaction:
     if transaction_data.get('transaction_type','') == 'credit':
         if transaction_data.get('amount', 0) > 0:
             transaction_data['amount'] = transaction_data['amount'] * -1
             
-    transaction = tr.Transaction(
+    transaction = Transaction(
         transaction_date=transaction_data.get('transaction_date'),
         categoryid=transaction_data.get('categoryid', 1),
         merchant_name=transaction_data.get('merchant_name', ''),
@@ -20,13 +20,13 @@ def insert_transaction(transaction_data: dict) -> tr.Transaction:
         
     return transaction
 
-def get_transaction_by_id(transactionid: int) -> tr.Transaction:
-    transaction = tr.Transaction.query.filter(tr.Transaction.transactionid == transactionid).one_or_none()
+def get_transaction_by_id(transactionid: int) -> Transaction:
+    transaction = Transaction.query.filter(Transaction.transactionid == transactionid).one_or_none()
     
     return transaction
 
 def get_transaction_by_date(date: str):
-    transactions = tr.Transaction.query.filter(tr.Transaction.transaction_date == date)
+    transactions = Transaction.query.filter(Transaction.transaction_date == date)
     transaction_dump = [transaction.to_json() for transaction in transactions]
     
     return transaction_dump
