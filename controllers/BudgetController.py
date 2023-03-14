939df8bbe9a7c7d3f563c0c9cfac909ac1c68dd4
Sellerman <Jeffrey.Sellers@maritz.com>
2023-03-14 10:43:16 -0400

    new controllers

---
+++ controllers/BudgetController.py
@@ -0,0 +1,32 @@
+from app import db
+from controllers.models import Budget
+from typing import Any
+
+def get_budget_by_id(budgetid: int) -> Budget:
+    
+    return Budget
+
+def get_budget_by_name(budget_name: str) -> Budget:
+    
+    return Budget
+
+def get_budget_by_category_name(category_name: str) -> Budget:
+    
+    return Budget
+
+
+def associate_budget_category(budgetid: int, categoryid: int) -> Budget:
+    
+    return Budget
+
+def insert_budget(budget_data: dict) -> Budget:
+    
+    return Budget
+
+def update_budget(budget_data: dict) -> Budget:
+    
+    return Budget
+
+def delete_budget(budget_id: int) -> None:
+    
+    return None
---
+++ controllers/CategoryController.py
@@ -0,0 +1,52 @@
+from app import db
+from controllers.models import Category
+from typing import Any
+
+def get_category_by_id(categoryid: int) -> Category:
+    category: Category = Category.query.filter(Category.categoryid == categoryid).one_or_none()
+    
+    if category == None:
+        raise ValueError(f"{categoryid} is not a valid category id.")
+    
+    return category
+
+def get_category_by_name(category_name: str) -> Category:
+    category: Category = Category.query.filter(Category.category_name == category_name).one_or_none()
+    
+    if category == None:
+        raise ValueError(f"{category_name} does not exist.")
+    
+    return category
+    
+
+def insert_category(category_name: str)-> Category:
+    category: Category = Category(category_name = category_name)
+    
+    db.session.add(category)
+    db.commit()
+    
+    return category
+
+def update_category(category_data: dict) -> Category:
+    category: Category = Category.query.filter(Category.categoryid == category_data.get('categoryid')).one_or_none()
+    
+    if category == None:
+        raise ValueError(f"{category_data.get('categoryid')} is not a valid category id.")
+    
+    if category_data.get('category_name') != category.category_name:
+        category.category_name = category_data.get('category_name', '')
+    
+    db.commit()
+    
+    return category
+
+def delete_category(categoryid: int) -> None:
+    category: Category = Category.query.filter(Category.categoryid == categoryid).one_or_none()
+    
+    if category == None:
+        raise ValueError(f"{categoryid} is not a valid category id.")
+    
+    db.session.delete(category)
+    db.commit()
+     
+     
