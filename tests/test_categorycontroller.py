81a0cd5fdc05aa0e234b6ce017e45c2108188bcd
Sellerman <Jeffrey.Sellers@maritz.com>
2023-03-21 09:25:43 -0400

    testing file for category controller

---
+++ tests/test_categorycontroller.py
@@ -0,0 +1,13 @@
+from controllers import CategoryController
+from app import app
+
+def test_category_insert():
+    test_category = "PyTest Category"
+    
+    with app.app_context():
+        CategoryController.insert_category(test_category).to_json()
+        new_category = CategoryController.get_category_by_name(test_category).to_json()
+    
+    assert new_category.get('category_name') == test_category.lower()
+    
+    
