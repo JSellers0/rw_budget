e72f12132878e484a751659908963c18deeeab07
Sellerman <Jeffrey.Sellers@maritz.com>
2023-03-22 10:50:56 -0400

    add account model and function to build standard accounts

--- controllers/models.py
+++ controllers/models.py
@@ -82,6 +82,73 @@ class Budget(db.Model): # type: ignore
             "budget_amount": self.budget_amount
         }
 
+class Account(db.Model): # type: ignore
+    __tablename__ = 'account'
+    accountid: Column = Column(Integer, primary_key=True)
+    account_name: Column = Column(String(200), nullable=False)
+    account_type: Column = Column(String(100), nullable=False)
+    rewards_features: Column = Column(String(300))
+    payment_day: Column = Column(String(50))
+    statement_day: Column = Column(String(50))
+    
+    def __repr__(self) -> str:
+        return "account({},{})".format(self.accountid, self.account_name)
+    
+    def to_json(self) -> dict[str, Column]:
+        return {
+            "accountid": self.accountid,
+            "account_name": self.account_name,
+            "account_type": self.account_type,
+            "rewards_features": self.rewards_features,
+            "payment_day": self.payment_day,
+            "statement_day": self.statement_day
+        }
+        
+    def to_tuple(self) -> tuple[Column, Column]:
+        return (self.accountid, self.account_name)
+    
+def build_accounts():
+    barlcays = Account(
+        account_name='Barclays',
+        account_type='Credit Card',
+        rewards_features='Restaurants 3x - Streaming/Phone/Internet 2x - Grocery Stores (except Target and Walmart) 2x',
+        payment_day='3rd',
+        statement_day='6th'
+        )
+    pnc_cash = Account(
+        account_name='PNC Rewards',
+        account_type='Credit Card',
+        rewards_features='Gas 4x - Restaurants 3x - Groceries 2x - 8,000 multiplier cap',
+        payment_day='2nd',
+        statement_day='7th'
+        )
+    pnc_spend = Account(
+        account_name='PNC Spend',
+        account_type='Checking Account',
+        rewards_features='',
+        payment_day='',
+        statement_day=''
+        )
+    quicksilver = Account(
+        account_name='Quicksilver',
+        account_type='Credit Card',
+        rewards_features='1.5x all purchases',
+        payment_day='5th',
+        statement_day='11th'
+        )
+    venture = Account(
+        account_name='Venture',
+        account_type='Credit Card',
+        rewards_features='5x hotels and car rentsl booked through CapOne - 1.25x all purchases',
+        payment_day='10th',
+        statement_day='16th'
+        )
+    
+    
+    with app.app_context():
+        db.session.add_all([venture,pnc_cash,pnc_spend,quicksilver,barlcays])
+        db.session.commit()
+    
 # Merchant
 
 # MerchantAlias - enable aliased merchant display
@@ -95,4 +162,6 @@ if __name__ == '__main__':
             db.create_all()
             db.session.execute("INSERT INTO category(category_name) VALUES ('Uncategorized');")
             db.session.commit()
+            
+        build_accounts()
             
