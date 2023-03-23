5542a7643f5605b6179d2c0099149af2ff23c47b
Sellerman <Jeffrey.Sellers@maritz.com>
2023-03-23 13:08:49 -0400

    use transaction_interface accept form data for insert and update new todo items

--- controllers/TransactionController.py
+++ controllers/TransactionController.py
@@ -1,50 +1,44 @@
 from app import db
-from controllers.models import Transaction, Category
+from controllers.models import Transaction, Transaction_Interface
+from controllers.forms import TransactionForm
 from datetime import date
 from typing import Any
 
-def get_all_transactions() -> list[dict[str, Any]]:
-    transactions = Transaction.query.order_by(Transaction.transaction_date.asc()).all()
-    transaction_dump = [transaction.to_json(transaction.category.category_name) for transaction in transactions]
-    
-    return transaction_dump
+# ToDo: Should insert and update accept data_type parameter and adjust accordingly or should there be one method for each type of data?
 
-def get_transaction_by_id(transactionid: int) -> Transaction:
+def get_all_transactions() -> list[Transaction_Interface]:
+    transactions = Transaction.query.order_by(Transaction.transaction_date.asc()).all()   
+    return [Transaction_Interface(transaction, transaction.category, transaction.account) for transaction in transactions]
+
+def get_transaction_by_id(transactionid: int) -> Transaction_Interface:
     transaction = Transaction.query.filter(Transaction.transactionid == transactionid).one_or_none()
     
     if transaction == None:
         raise ValueError(f"{transactionid} is not a valid transaction id.")
     
-    return transaction
+    return Transaction_Interface(transaction, transaction.category, transaction.account)
 
-def get_transaction_by_date(date: str) -> list[dict[str, Any]]:
-    transactions = Transaction.query.filter(Transaction.transaction_date == date)
-    transaction_dump = [transaction.to_json(transaction.category.category_name) for transaction in transactions]
+def get_transaction_by_date(date: str) -> list[Transaction_Interface]:
+    transactions = Transaction.query.filter(Transaction.transaction_date == date).all()
     
-    if len(transaction_dump) == 0:
+    if len(transactions) == 0:
         raise ValueError(f"{date} does not have any transactions recorded.")
     
-    return transaction_dump
+    return [Transaction_Interface(transaction, transaction.category, transaction.account) for transaction in transactions]
 
-def get_transaction_by_date_range(start: str, end: str) -> list[Any]:
-    transactions = Transaction.query.filter(Transaction.transaction_date <= end).filter(Transaction.transaction_date >= start)
-    transaction_dump = [transaction.to_json(transaction.category.category_name) for transaction in transactions]
+def get_transaction_by_date_range(start: str, end: str) -> list[Transaction_Interface]:
+    transactions = Transaction.query.filter(Transaction.transaction_date <= end).filter(Transaction.transaction_date >= start).all()
+    return [Transaction_Interface(transaction, transaction.category, transaction.account) for transaction in transactions]
     
-    return transaction_dump
-    
-def get_transaction_by_category(categoryid: int) -> list[Any]:
-    transactions = Transaction.query.filter(Transaction.categoryid <= categoryid)
-    transaction_dump = [transaction.to_json(transaction.category.category_name) for transaction in transactions]
-    
-    return transaction_dump
+def get_transaction_by_category(categoryid: int) -> list[Transaction_Interface]:
+    transactions = Transaction.query.filter(Transaction.categoryid == categoryid).all()
+    return [Transaction_Interface(transaction, transaction.category, transaction.account) for transaction in transactions]
 
-def get_twenty_recent_transactions() -> list[Any]:
+def get_twenty_recent_transactions() -> list[Transaction_Interface]:
     transactions = Transaction.query.order_by(Transaction.transaction_date.desc()).limit(20)
-    transaction_dump = [transaction.to_json(transaction.category.category_name) for transaction in transactions]
+    return [Transaction_Interface(transaction, transaction.category, transaction.account) for transaction in transactions]
     
-    return transaction_dump
-    
-def insert_transaction(transaction_data: dict) -> Transaction:
+def insert_transaction(transaction_data: dict) -> Transaction_Interface:
     # Make sure credit values are negative
     if transaction_data.get('transaction_type','') == 'credit':
         if transaction_data.get('amount', 0) > 0:
@@ -52,6 +46,7 @@ def insert_transaction(transaction_data: dict) -> Transaction:
                 
     transaction: Transaction = Transaction(
         transaction_date=transaction_data.get('transaction_date', date.today()),
+        accountid=transaction_data.get('accountid', 1),
         categoryid=transaction_data.get('categoryid', 1),
         merchant_name=transaction_data.get('merchant_name', ''),
         transaction_type=transaction_data.get('transaction_type', ''),
@@ -62,9 +57,30 @@ def insert_transaction(transaction_data: dict) -> Transaction:
     db.session.add(transaction)    
     db.session.commit()
         
-    return transaction
+    return Transaction_Interface(transaction, transaction.category, transaction.account)
 
-def update_transaction(transaction_data: dict) -> Transaction:
+def insert_transaction_form(transaction_data: TransactionForm) -> Transaction_Interface:
+    # Make sure credit values are negative
+    if transaction_data.transaction_type.data == 'credit':
+        if transaction_data.amount.data > 0: # type: ignore
+            transaction_data.amount = transaction_data.amount.data * -1 # type: ignore
+                
+    transaction: Transaction = Transaction(
+        transaction_date=transaction_data.transaction_date.data,
+        accountid = transaction_data.account.data,
+        categoryid=transaction_data.category.data,
+        merchant_name=transaction_data.merchant_name.data,
+        transaction_type=transaction_data.transaction_type.data,
+        amount=transaction_data.amount.data,
+        note=transaction_data.note.data
+    )
+
+    db.session.add(transaction)    
+    db.session.commit()
+        
+    return Transaction_Interface(transaction, transaction.category, transaction.account)
+
+def update_transaction(transaction_data: dict) -> Transaction_Interface:
     transaction: Transaction = Transaction.query.filter(Transaction.transactionid == transaction_data.get('transactionid')).one_or_none()
     
     if transaction == None:
@@ -85,7 +101,30 @@ def update_transaction(transaction_data: dict) -> Transaction:
 
     db.session.commit()
     
-    return transaction
+    return Transaction_Interface(transaction, transaction.category, transaction.account)
+
+def update_transaction_form(transaction_data: TransactionForm) -> Transaction_Interface:
+    transaction: Transaction = Transaction.query.filter(Transaction.transactionid == transaction_data.transactionid.data).one_or_none()
+    
+    if transaction == None:
+        raise ValueError(f"{transaction_data.transactionid.data} is not a valid transaction id.")
+    
+    if transaction.transaction_date != transaction_data.transaction_date.data:
+        transaction.transaction_date = transaction_data.transaction_date.data # type: ignore
+    if transaction_data.category.data != transaction.categoryid:
+        transaction.categoryid = transaction_data.category.data # type: ignore
+    if transaction_data.merchant_name.data != transaction.merchant_name:
+        transaction.merchant_name = transaction_data.merchant_name.data
+    if transaction_data.transaction_type.data != transaction.transaction_type:
+        transaction.transaction_type = transaction_data.transaction_type.data  # type: ignore
+    if transaction_data.amount.data != transaction.amount:
+        transaction.amount = transaction_data.amount.data  # type: ignore
+    if transaction_data.note.data != transaction.note:
+        transaction.note = transaction_data.note.data
+
+    db.session.commit()
+    
+    return Transaction_Interface(transaction, transaction.category, transaction.account)
 
 def delete_transaction(transactionid: int)-> None:
     transaction = Transaction.query.filter(Transaction.transactionid == transactionid).one_or_none()
@@ -94,4 +133,5 @@ def delete_transaction(transactionid: int)-> None:
         raise ValueError(f"{transactionid} is not a valid transaction id.")
     
     db.session.delete(transaction)
-    db.commit()
+    db.session.commit()
+    # ToDo: Does transaction object still exist and can it be assigned to Transaction_Interface?  Should it be returned?
