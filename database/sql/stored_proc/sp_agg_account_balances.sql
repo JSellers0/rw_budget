76cd3583d9568748e36aa3b48dc51f470328555b
Sellerman <Jeffrey.Sellers@maritz.com>
2023-05-07 10:55:11 -0400

    better agg_period identification

--- database/sql/stored_proc/sp_agg_account_balances.sql
+++ database/sql/stored_proc/sp_agg_account_balances.sql
@@ -1,44 +1,31 @@
 CREATE OR REPLACE PROCEDURE sp_agg_account_balances()
-BEGIN
-    DECLARE ed DATE;
+this_proc:BEGIN
+    
     DECLARE sd DATE;
+    DECLARE ed DATE;
 
-    SET ed = Date_Add(CURDATE(), INTERVAL -1 DAY);
     IF Extract(DAY FROM CURDATE()) = 1 THEN
-        SET sd = ConCat(Extract(YEAR FROM @ed), '-', Extract(MONTH FROM @ed), '-15');
-    ELSEIF Extract(DAY FROM CURDATE()) = 15 THEN
-        SET sd = Date_Add(CURDATE(), INTERVAL -14 DAY);
+        SET sd = Date_Add(CURDATE(), INTERVAL -1 MONTH);
+        SET ed = Date_Add(CURDATE(), INTERVAL -1 DAY);
+    ELSE
+        LEAVE this_proc;
     END IF;
 
-    CREATE OR REPLACE TEMPORARY TABLE agg_bal (
-        accountid INTEGER, balance DECIMAL(7,2)
-    );
-
-    INSERT INTO agg_bal (accountid, balance)
-    SELECT accountid,  sum(balance) as balance
+    INSERT INTO accountbalance (accountid, balance, agg_start, agg_end)
+    SELECT
+        accountid, sum(balance), sd, ed
     FROM (
         SELECT
             t.accountid, sum(t.amount) AS balance
         FROM transaction t
-        WHERE t.transaction_date BETWEEN @sd AND @ed
+        WHERE t.transaction_date BETWEEN sd AND ed
         GROUP BY t.accountid
         UNION
         SELECT
             ab.accountid, ab.balance
         FROM accountbalance ab
-        WHERE ab.is_current = 1
+        WHERE ab.agg_start = Date_Add(sd, INTERVAL -1 MONTH)
     ) bal_data
-    GROUP BY accountid;
-
-    UPDATE accountbalance
-    SET is_current = 0;
-
-    INSERT INTO accountbalance (accountid, balance, is_current, agg_period)
-    SELECT
-        accountid, balance, 1, ConCat(@sd, '-', @ed)
-    FROM agg_bal
-    ;
-
-    DROP TABLE agg_bal
+    GROUP BY accountid
     ;
 END;
