3d80c20f9e0e649a98e91e601f22d7046112d6cb
Sellerman <Jeffrey.Sellers@maritz.com>
2023-05-15 18:43:17 -0400

    cashflow summary uses cashflow date

--- controllers/TransactionController.py
+++ controllers/TransactionController.py
@@ -357,19 +357,16 @@ def get_cashflow_df(start:str, end:str) -> dict:
     cashflow_sql = f"""
     WITH chk AS (
         SELECT
-            Sum(DISTINCT Coalesce(ab.balance, 0)) AS chk_balance
-            , Sum(CASE WHEN transaction_type = 'debit' AND Extract(DAY FROM transaction_date) < 15 
-                THEN amount ELSE 0 END) AS chk_in_top
-            , Sum(CASE WHEN transaction_type = 'credit'  AND Extract(DAY FROM transaction_date) < 15 
-                THEN amount ELSE 0 END) AS chk_out_top
-            , Sum(CASE WHEN transaction_type = 'debit' AND Extract(DAY FROM transaction_date) > 14 
-                THEN amount ELSE 0 END) AS chk_in_bot
-            , Sum(CASE WHEN transaction_type = 'credit'  AND Extract(DAY FROM transaction_date) > 14 
-                THEN amount ELSE 0 END) AS chk_out_bot
+            Sum(CASE WHEN transaction_type = 'debit' AND Extract(DAY FROM cashflow_date) < 15 
+                THEN amount END) AS chk_in_top
+            , Sum(CASE WHEN transaction_type = 'credit'  AND Extract(DAY FROM cashflow_date) < 15 
+                THEN amount END) AS chk_out_top
+            , Sum(CASE WHEN transaction_type = 'debit' AND Extract(DAY FROM cashflow_date) > 14 
+                THEN amount END) AS chk_in_bot
+            , Sum(CASE WHEN transaction_type = 'credit'  AND Extract(DAY FROM cashflow_date) > 14 
+                THEN amount END) AS chk_out_bot
         FROM transactions t
-            LEFT JOIN accountbalance ab ON ab.accountid = t.accountid
-            AND ab.agg_start = Date_Add('{start}', INTERVAL - 1 MONTH)
-        WHERE transaction_date BETWEEN '{start}' AND '{end}'
+        WHERE cashflow_date BETWEEN '{start}' AND '{end}'
             AND t.accountid IN (
             SELECT accountid FROM account
             WHERE account_type = 'Checking'
@@ -380,49 +377,37 @@ def get_cashflow_df(start:str, end:str) -> dict:
             )
         ), card AS (
         SELECT
-            Sum(DISTINCT Coalesce(ab.balance, 0)) AS card_balance
-            , Sum(CASE WHEN transaction_type = 'debit' AND Extract(DAY FROM transaction_date) < 15 
-                THEN amount ELSE 0 END) AS card_in_top
-            , Sum(CASE WHEN transaction_type = 'credit'  AND Extract(DAY FROM transaction_date) < 15 
-                THEN amount ELSE 0 END) AS card_out_top
-            , Sum(CASE WHEN transaction_type = 'debit' AND Extract(DAY FROM transaction_date) > 14 
-                THEN amount ELSE 0 END) AS card_in_bot
-            , Sum(CASE WHEN transaction_type = 'credit'  AND Extract(DAY FROM transaction_date) > 14 
-                THEN amount ELSE 0 END) AS card_out_bot
+            Sum(CASE WHEN transaction_type = 'debit' AND Extract(DAY FROM cashflow_date) < 15 
+                THEN amount END) AS card_in_top
+            , Sum(CASE WHEN transaction_type = 'credit'  AND Extract(DAY FROM cashflow_date) < 15 
+                THEN amount END) AS card_out_top
+            , Sum(CASE WHEN transaction_type = 'debit' AND Extract(DAY FROM cashflow_date) > 14 
+                THEN amount END) AS card_in_bot
+            , Sum(CASE WHEN transaction_type = 'credit'  AND Extract(DAY FROM cashflow_date) > 14 
+                THEN amount END) AS card_out_bot
         FROM transactions t
-            LEFT JOIN accountbalance ab ON ab.accountid = t.accountid
-            AND ab.agg_start = Date_Add('{start}', INTERVAL - 1 MONTH)
-        WHERE transaction_date BETWEEN '{start}' AND '{end}'
+        WHERE cashflow_date BETWEEN '{start}' AND '{end}'
             AND t.accountid IN (
             SELECT accountid FROM account
             WHERE account_type = 'Credit Card'
             )
         ), summary AS (
         SELECT
-            chk_balance + chk_in_top + chk_in_bot AS cash_in_sum
-            , card_balance + chk_out_top + chk_out_bot + card_out_top + card_out_bot AS cash_out_sum
-            , (chk_balance + chk_in_top + chk_in_bot) + (card_balance + chk_out_top + chk_out_bot + card_out_top + card_out_bot) AS cash_remain_sum
+            # Summary
+            (chk_in_top + chk_in_bot) + (chk_out_top + chk_out_bot + card_out_top + card_out_bot) AS cash_remain_sum
+            , chk_in_top + chk_in_bot AS cash_in_sum
+            , chk_out_top + chk_out_bot + card_out_top + card_out_bot AS cash_out_sum
+            # Top
+            , chk_in_top + chk_out_top + card_out_top AS cash_remain_top
+            , chk_in_top AS cash_in_top
+            , chk_out_top + card_out_top AS cash_out_top
+            # Bot
+            , chk_in_bot + chk_out_bot + card_out_bot AS cash_remain_bot
+            , chk_in_bot AS cash_in_bot
+            , chk_out_bot + card_out_bot AS cash_out_bot
         FROM chk, card
-        ), top AS (
-        SELECT
-            chk_balance + chk_in_top AS cash_in_top
-            , card_balance + chk_out_top + card_out_top AS cash_out_top
-            , chk_balance + chk_in_top + card_balance + chk_out_top + card_out_top AS cash_remain_top
-            , card_balance + card_in_top + card_out_top AS card_top_balance
-        FROM chk, card
-        ), top_bot AS (
-        SELECT
-            top.*
-            , cash_remain_top + chk.chk_in_bot AS cash_in_bot
-            , top.card_top_balance + card.card_out_bot + chk.chk_out_bot AS cash_out_bot
-            , (cash_remain_top + chk.chk_in_bot) + (top.card_top_balance + card.card_out_bot + chk.chk_out_bot) AS cash_remain_bot
-        FROM top, chk, card
         )
-        SELECT 
-            cash_remain_sum, cash_in_sum, cash_out_sum
-            , cash_remain_top, cash_in_top, cash_out_top
-            , cash_remain_bot, cash_in_bot, cash_out_bot
-        FROM summary, top_bot
+        SELECT * FROM summary
         ;"""
     
     results = db.session.execute(text(cashflow_sql))
