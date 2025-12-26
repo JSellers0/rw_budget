644c3922f133e633d78c4c1f3e35f4e12f27fec3
JSellers0@Candyland <jsellers0@CandyLand.localdomain>
2025-12-26 14:19:03 -0500

    fix: runs locally

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import os
-
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-#from flask_bcrypt import Bcrypt
-#from flask_login import LoginManager
-
 from logging_config import setup_logging
+
+load_dotenv()
 
 app: Flask = Flask(__name__)
 
 app.secret_key = os.environ.get("SECRET_KEY")
-
 db_uri = os.environ.get("DATABASE_URL")
 
 app.config["SQLALCHEMY_ECHO"] = True
@@ -20,9 +17,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db: SQLAlchemy = SQLAlchemy(app)
 
-setup_logging(app)
+if os.environ.get("FLASK_ENV", "development") == "production":
+    setup_logging(app)
     
 app.logger.info('Application started')
-#bcrypt: Bcrypt = Bcrypt()
-#bcrypt.init_app(app)
 
--- rw_budget.wsgi
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0, "/var/www/flask/rw_budget/")
-
-from routes import app
-
-application = app
